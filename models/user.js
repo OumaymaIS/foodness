c6180adbfecee9ce473fea451ff43bcedb4d1073
medabbassiEsprit <106880027+medabbassiEsprit@users.noreply.github.com>
2023-05-29 20:35:54 +0100

    Update user.js

---
+++ models/user.js
@@ -0,0 +1,10 @@
+
+// this an exemple to follow 
+const mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var User = new Schema({
+  Username: String,
+  Password: String,
+  Email: String
+});
+module.exports = mongoose.model("user", User);
